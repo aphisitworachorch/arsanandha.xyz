f3624e6623f6324af3dfbd700d56d868961af720
Asanan Aphisitworachorch <arsanandha.ap@gmail.com>
2021-06-14 17:34:47 +0700

    cloudflare real ip

--- app/Http/Middleware/Logger.php
+++ app/Http/Middleware/Logger.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Str;
 use Jenssegers\Agent\Agent;
+use Molayli\CloudflareRealIpServiceProvider;
 
 class Logger
 {
@@ -42,7 +43,8 @@ class Logger
         $random = Str::random(9);
         Cache::increment('visit_today');
         Redis::select(8);
-        Redis::hSet("visit_log_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'header_http',json_encode($body['header']));
+        $cf_ip = CloudflareRealIpServiceProvider::ip ();
+        Redis::hSet("visit_log_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'header_http',json_encode($body['header']),'real_ip',$cf_ip);
 //        Cache::tags("visit_log_{$ip}")->put("visit_{$random}",json_encode($body),86400);
     }
 }
