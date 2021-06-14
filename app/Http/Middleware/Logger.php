3ab31372d522fd0b09b30d61cee2b0ec9ba79182
Asanan Aphisitworachorch <arsanandha.ap@gmail.com>
2021-06-14 10:55:28 +0700

    newer fresher

--- app/Http/Middleware/Logger.php
+++ app/Http/Middleware/Logger.php
@@ -33,7 +33,8 @@ class Logger
         );
         $random = Str::random(9);
         Cache::increment('visit_today');
-        Redis::command("HMSET visit_log_{$ip} ip_address '{$ip}' user_agent '{$body['user_agent']}' header_http '{$body['header']}'");
+//        Redis::command("HMSET visit_log_{$ip} ip_address '{$ip}' user_agent '{$body['user_agent']}' header_http '{$body['header']}'");
+        Redis::hSet("visit_log_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'header_http',json_encode($body['header']));
 //        Cache::tags("visit_log_{$ip}")->put("visit_{$random}",json_encode($body),86400);
     }
 }
