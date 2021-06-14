f097bd57915f9be3a4b32b48be748d517a4d23a9
Asanan Aphisitworachorch <arsanandha.ap@gmail.com>
2021-06-14 17:37:12 +0700

    get one

--- app/Http/Middleware/Logger.php
+++ app/Http/Middleware/Logger.php
@@ -27,7 +27,6 @@ class Logger
             "ip_address"=>$ip,
             "user_agent"=>$agent->getUserAgent ()
         );
-        Redis::select(7);
         Redis::hSet("request_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'request_body',json_encode($request->all()));
         return $next($request);
     }
@@ -42,7 +41,6 @@ class Logger
         );
         $random = Str::random(9);
         Cache::increment('visit_today');
-        Redis::select(8);
         $cf_ip = CloudflareRealIpServiceProvider::ip ();
         Redis::hSet("visit_log_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'header_http',json_encode($body['header']),'real_ip',$cf_ip);
 //        Cache::tags("visit_log_{$ip}")->put("visit_{$random}",json_encode($body),86400);
