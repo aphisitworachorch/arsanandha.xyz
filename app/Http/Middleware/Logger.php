d8a2588f60cbec691e0b6fd16d353c5df334f150
Asanan Aphisitworachorch <arsanandha.ap@gmail.com>
2021-06-14 17:43:16 +0700

    proper

--- app/Http/Middleware/Logger.php
+++ app/Http/Middleware/Logger.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Str;
@@ -12,6 +13,15 @@ use Molayli\CloudflareRealIpServiceProvider;
 
 class Logger
 {
+    private string $date;
+    private string $random;
+
+    public function __construct ()
+    {
+        $this->date = Carbon::now()->toDateString();
+        $this->random = Str::random(16);
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -27,7 +37,7 @@ class Logger
             "ip_address"=>$ip,
             "user_agent"=>$agent->getUserAgent ()
         );
-        Redis::hSet("request_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'request_body',json_encode($request->all()));
+        Redis::hSet("request_log_:{$this->date}:{$this->random}",'ip_address',$ip,'user_agent',$body['user_agent'],'request_body',json_encode($request->all()));
         return $next($request);
     }
     public function terminate($request)
@@ -39,10 +49,9 @@ class Logger
             "user_agent"=>$agent->getUserAgent (),
             "header"=>$agent->getHttpHeaders ()
         );
-        $random = Str::random(9);
         Cache::increment('visit_today');
         $cf_ip = CloudflareRealIpServiceProvider::ip ();
-        Redis::hSet("visit_log_{$ip}",'ip_address',$ip,'user_agent',$body['user_agent'],'header_http',json_encode($body['header']),'real_ip',$cf_ip);
+        Redis::hSet("visit_log:{$this->date}:{$this->random}",'ip_address',$ip,'user_agent',$body['user_agent'],'header_http',json_encode($body['header']),'real_ip',$cf_ip);
 //        Cache::tags("visit_log_{$ip}")->put("visit_{$random}",json_encode($body),86400);
     }
 }
